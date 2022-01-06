5614b80ffd217247c1b651696262ab785e56b838
Shweta Kawar <85010865+kawarshweta@users.noreply.github.com>
2022-01-06 23:51:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const accordion_items_elms = document.querySelectorAll('.accordion .accordion__item');
+const accordion__item__head = document.querySelector(".accordion__item__head");
+
+accordion_items_elms.forEach(accordionItem => {
+    console.log(accordionItem);
+  accordionItem.querySelector('.accordion__item__head').addEventListener('click', (e) => {
+    accordionItem.classList.toggle('open');
+    
+    accordion__item__head.classList.toggle('add_color')
+  });
+
+});
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+
+one.onclick = function() {
+    one.classList.add("active");
+    two.classList.remove("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
+    five.classList.remove("active");
+}
+
+two.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
+    five.classList.remove("active");
+}
+three.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.add("active");
+    four.classList.remove("active");
+    five.classList.remove("active");
+}
+four.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.add("active");
+    four.classList.add("active");
+    five.classList.remove("active");
+}
+five.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.add("active");
+    four.classList.add("active");
+    five.classList.add("active");
+}
